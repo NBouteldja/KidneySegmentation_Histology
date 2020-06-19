ce6e36a46883c8fec38e4473c199cef9c7d56dc1
NBouteldja <40466985+NBouteldja@users.noreply.github.com>
2020-06-19 10:58:40 +0200

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,7 @@
 import skimage as ski
 import numpy as np
 
+# this class evaluates prediction performance of a single class/label using instace dice scores as well as average precisions
 class ClassEvaluator(object):
 
     def __init__(self, thres=None):
@@ -10,6 +11,7 @@ class ClassEvaluator(object):
         self.FP = np.zeros(self.thres.__len__())
         self.diceScores = []
 
+    # add prediction/ground-truth pair
     def add_example(self, pred, gt):
         gt_num = len(np.unique(gt)) - 1
         IoU_dict = []  # (prediction label)-(IoU)
@@ -19,6 +21,7 @@ class ClassEvaluator(object):
         gt_area = self.get_area_dict(gt)
         unique = np.unique(pred)
 
+        # compute dice scores of each predicted instance with its maximally overlapping ground-truth instance
         for label in unique:
             if label == 0:
                 continue
@@ -38,6 +41,7 @@ class ClassEvaluator(object):
                 diceScore = 2*intersect / (pred_area[label] + gt_area[u[i]])
                 self.diceScores.append(diceScore)
 
+        # count all TP, FP, FN in current image
         IoU_dict = np.array(IoU_dict)
         for i, threshold in enumerate(self.thres):
             tp = np.sum(IoU_dict > threshold)
@@ -45,7 +49,7 @@ class ClassEvaluator(object):
             self.FN[i] += gt_num - tp
             self.TP[i] += tp
 
-
+        # also compute dice scores of each ground-truth instance with its maximally overlapping prediction instance
         uniqueGT = np.unique(gt)
         for label in uniqueGT:
             if label == 0:
@@ -61,12 +65,12 @@ class ClassEvaluator(object):
                 diceScore = 2 * intersect / (gt_area[label] + pred_area[u[i]])
                 self.diceScores.append(diceScore)
 
-
+    # measure area regions
     def get_area_dict(self, label_map):
         props = ski.measure.regionprops(label_map)
         return {p.label: p.area for p in props}
 
-
+    # compute average precision for each threshold
     def score(self):
         precisions = self.TP / (self.TP + self.FN + self.FP)
         avg_precision = np.mean(precisions)
